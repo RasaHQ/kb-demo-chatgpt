62a81b3109dc84f92fee0c59f91fd60d84d2c4cf
sonam pankaj <sonampankaj2@gmail.com>
2023-06-13 16:19:17 +0200

    initial commit

--- actions/actions.py
+++ actions/actions.py
@@ -5,27 +5,34 @@ import requests
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-import openai
-from dotenv import load_dotenv
 
+
+class RestaurantAPI(object):
+
+    def __init__(self):
+        self.db = pd.read_csv("restaurants.csv")
+
+    def fetch_restaurants(self):
+        return self.db.head()
+
+    def format_restaurants(self, df, header=True) -> Text:
+        return df.to_csv(index=False, header=header)
 
 
 class ChatGPT(object):
 
     def __init__(self):
-        load_dotenv()
         self.url = "https://api.openai.com/v1/chat/completions"
         self.model = "gpt-3.5-turbo"
         self.headers={
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {os.getenv('Open_API_key')}"
-            
+            "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}"
         }
-        
+        self.prompt = "Answer the following question, based on the data shown. " \
+            "Answer in a complete sentence and don't say anything else."
 
-    def ask(self, article, question):
-        prompt =f" Give a desciption for the text delimited by  triple backticks ```{article}``` "
-        content  = prompt + '/n/n' + article + '/n/n' + question
+    def ask(self, restaurants, question):
+        content  = self.prompt + "\n\n" + restaurants + "\n\n" + question
         body = {
             "model":self.model, 
             "messages":[{"role": "user", "content": content}]
@@ -35,31 +42,37 @@ class ChatGPT(object):
             headers=self.headers,
             json=body,
         )
-        #print(result)
         return result.json()["choices"][0]["message"]["content"]
 
-
-
+restaurant_api = RestaurantAPI()
 chatGPT = ChatGPT()
 
-class ActionDescribe(Action):
+class ActionShowRestaurants(Action):
 
     def name(self) -> Text:
-        return "action_describe"
+        return "action_show_restaurants"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        question = tracker.latest_message["text"]
-        print(os.getenv('Open_API_key'))
-        # summary.append(question)
-        # content = (','.join(summary))
-        
 
-        with open("test.txt") as file:
-            lines = file.readlines()
-        article = '\n'.join(lines)
-        print(article)
-        reply_content = chatGPT.ask(article, question)
-        print(reply_content)
-        dispatcher.utter_message(text=f"Here are some description\n{reply_content}")
+        restaurants = restaurant_api.fetch_restaurants()
+        results = restaurant_api.format_restaurants(restaurants)
+        readable = restaurant_api.format_restaurants(restaurants[['Restaurants', 'Rating']], header=False)
+        dispatcher.utter_message(text=f"Here are some restaurants:\n\n{readable}")
+
+        return [SlotSet("results", results)]
+
+
+class ActionRestaurantsDetail(Action):
+    def name(self) -> Text:
+        return "action_restaurants_detail"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        previous_results = tracker.get_slot("results")
+        question = tracker.latest_message["text"]
+        answer = chatGPT.ask(previous_results, question)
+        dispatcher.utter_message(text = answer)
